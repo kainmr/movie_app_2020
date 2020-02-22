c69c84bf6a2e4078d72c574d5ad008df9303a2cf
kainmr <kainmr410@gmail.com>
2020-02-22 11:47:47 +0800

    Updates

---
+++ precache-manifest.0d0c2eccbebddb89b53a067ded46aaf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edbd099f141657357429bc3cc065161a",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "8c18a4a6d9ea42d861a2",
+    "url": "/movie_app_2020/static/css/main.810fcd88.chunk.css"
+  },
+  {
+    "revision": "4e09b8d67742e4a83064",
+    "url": "/movie_app_2020/static/js/2.1ac01af5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app_2020/static/js/2.1ac01af5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c18a4a6d9ea42d861a2",
+    "url": "/movie_app_2020/static/js/main.dbec9fd3.chunk.js"
+  },
+  {
+    "revision": "01aae0007e1cfd873da8",
+    "url": "/movie_app_2020/static/js/runtime-main.482f7179.js"
+  }
+]);
